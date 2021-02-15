473f6fb9cc7c6ddd731d1b5b165e64fe96b73870
PatiGenius <you@example.com>
2021-02-15 19:39:51 +0100

    Change motif message

--- seqClass.py
+++ seqClass.py
@@ -27,7 +27,7 @@ else:
 
 if args.motif:
   args.motif = args.motif.upper()
-  print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+  print(f'Motif evaluation: looking for "{args.motif}" in sequence "{args.seq}"... ', end = '')
   if re.match(args.motif, args.seq):
     print("FOUND")
   else:
